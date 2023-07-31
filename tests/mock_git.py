802c1df89f59eb557afc79c29d7cb20792aa6dad
Robin van der Noord <robinvandernoord@gmail.com>
2023-07-31 23:00:39 +0200

    chore(pytest): fix mock_git

--- tests/mock_git.py
+++ tests/mock_git.py
@@ -15,6 +15,8 @@ def mock_git():
     assert pytest_examples.exists()
     with TempdirOrExistingDir() as cwd, chdir(cwd):
         git("init")
+        git("config", "--local", "user.email", "bot@su6.nl")
+        git("config", "--local", "user.name", "PyTest")
         git("config", "--local", "commit.gpgsign", "false")
 
         shutil.copy(pytest_examples / "magic_pre.py", "magic.py")
