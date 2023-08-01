6f41d47a0fe3e267d374211eb3591782424a73f2
Robin <robin.vdn@educationwarehouse.nl>
2023-08-01 11:13:46 +0200

    feat: expose core_ and handle_cli methods from the top level of the library

--- src/pydal2sql_core/__init__.py
+++ src/pydal2sql_core/__init__.py
@@ -9,10 +9,14 @@ Expose methods for the library.
 from .core import generate_sql
 from .helpers import get_typing_args
 from .types import SUPPORTED_DATABASE_TYPES as _SUPPORTED_DATABASE_TYPES
+from .cli_support import core_create, core_alter, handle_cli
 
 SUPPORTED_DATABASE_TYPES = get_typing_args(_SUPPORTED_DATABASE_TYPES)
 
 __all__ = [
     "generate_sql",
     "SUPPORTED_DATABASE_TYPES",
+    "core_create",
+    "core_alter",
+    "handle_cli",
 ]
